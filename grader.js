adb7865f2a6452afb9dd0ef6ceb6198c75566f21
bob <bob@fu.com>
2013-07-04 17:52:07 +0000

    Add more tests to grader script.
    Also include url command line option

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -36,16 +37,17 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var cheerioHtmlFile = function(htmlfile, asFile) {
+    var data = asFile == true ? fs.readFileSync(htmlfile) : htmlfile; 
+    return cheerio.load(data);
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(htmlfile, checksfile, asFile) {
+    $ = cheerioHtmlFile(htmlfile, asFile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -55,14 +57,31 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var processFiles = function(file, checksfile, asFile) {
+    var checkJson = checkHtmlFile(file, checksfile, asFile);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+}
+
 if(require.main == module) {
     program
         .option('-c, --checks ', 'Path to checks.json', assertFileExists, CHECKSFILE_DEFAULT)
         .option('-f, --file ', 'Path to index.html', assertFileExists, HTMLFILE_DEFAULT)
+        .option('-u, --url <file>', 'Url path')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (typeof(program.url) !== 'undefined') {
+	rest.get(program.url).on('complete', function(result) {
+	    if (result instanceof Error) {
+		console.log('Error: ' + result.message);
+		process.exit(1);
+	    } else {
+		processFiles(result, program.checks, false);
+		process.exit(1);
+	    }
+	});
+    } else {
+	processFiles(program.file, program.checks, true);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
